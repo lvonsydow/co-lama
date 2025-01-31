313cf6ad2a726edfbe0ac3410ccb423dc2adb87a
Ludvig von Sydow <ludvig.vonsydow@devoteam.com>
2025-01-31 22:28:57 +0100

    Update PySide6 URLs to use wheel files

--- Formula/co-lama.rb
+++ Formula/co-lama.rb
@@ -11,22 +11,22 @@ class CoLama < Formula
   depends_on "colima"
 
   resource "pyside6" do
-    url "https://files.pythonhosted.org/packages/f1/a8/b1c91d5f5a7d6e1f7c0c7d2e0d38c1d55f8cc1e2c7d5e8e5c3a3d2f1c5e6/PySide6-6.6.3.1-cp38-abi3-macosx_11_0_universal2.whl"
+    url "https://files.pythonhosted.org/packages/ba/9a/3483d05305701ba810192572cee5977ff884c033a1b8f96ab9582d81ccd4/PySide6-6.6.3.1-cp38-abi3-macosx_11_0_universal2.whl"
     sha256 "3d2ebb08a7744b59e1270e57f264a9ef5b45fccdc0328a9aeb50d890d6b3f4f2"
   end
 
   resource "pyside6-addons" do
-    url "https://files.pythonhosted.org/packages/source/P/PySide6_Addons/PySide6_Addons-6.6.3.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/51/3e/b77d2b9a1efcb5c90a2df4f51eb10bce45b3787c4fa16b69c599fd6620b9/PySide6_Addons-6.6.3.1-cp38-abi3-manylinux_2_31_aarch64.whl"
     sha256 "f7acd26fe8e1a745ef0be66b49ee49ee8ae50c2a2855d9792db262ebc7916d98"
   end
 
   resource "pyside6-essentials" do
-    url "https://files.pythonhosted.org/packages/source/P/PySide6_Essentials/PySide6_Essentials-6.6.3.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/af/90/3164ace42cb80ed55642e965934133d0c49bfa3ea79e43631dd331cdc866/PySide6_Essentials-6.6.3.1-cp38-abi3-win_amd64.whl"
     sha256 "d993989a10725c856f5b07f25e0664c5059daa92c259549c9df0972b5b0c7935"
   end
 
   resource "shiboken6" do
-    url "https://files.pythonhosted.org/packages/source/s/shiboken6/shiboken6-6.6.3.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/14/60/dc79d4ea59ed1ebe6062c5db972b31d489ea84315dcf3bd58a2a741c73b3/shiboken6-6.6.3.1-cp38-abi3-manylinux_2_28_x86_64.whl"
     sha256 "35936f06257e5c37ae8993da0cb5a528e5db3ea1fc2bb6b12cdf899a11510966"
   end
 
